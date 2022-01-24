21ad3596e10318470e837360ee8724e9c479bb3f
Rodrigo <rodrigo.navarro36@gmail.com>
2022-01-24 19:49:28 -0300

    Corrigindo o código e adicionando mais opções ao menu

--- lista_de_mercado.py
+++ lista_de_mercado.py
@@ -5,39 +5,62 @@ print('*-' * 30)
 print('{:=^60}'.format(' Lista de Compras '))
 print('*-' * 30)
 
-
-opcao = int(input("""Selecione a opção que você deseja: 
-[ 1 ] - Visualizar lista 
-[ 2 ] - Ver todas as listas
-[ 3 ] - Adicionar/remover itens na lista
-[ 4 ] - Sair"""))
+lista_atual = list()
+todas_listas = [lista_atual]
+invalid_input = True
 
 
+def start():
+    opcao = int(input("""Selecione a opção que você deseja: 
+        [ 1 ] - Visualizar lista 
+        [ 2 ] - Adicionar/remover itens na lista
+        [ 3 ] - Ver todas as listas
+        [ 4 ] - Sair """))
 
-lista_atual = ['banana']
-todas_listas = []
-produto = 0
+    if opcao == 1:
+        print(lista_atual)
+        escolha = input('Deseja voltar as opções? Pressione "S" para Sim e "N" para Não.').upper()
 
-if opcao == 1:
-    print(lista_atual)
-    escolha = input('Deseja voltar as opções? Pressione "S" para Sim e "N" para Não.').upper()
-    if escolha == "S":
-        start()
+        if escolha == "S":
+            start()
 
-    elif escolha == "N":
+        elif escolha == "N":
+            print('Finalizando programa, até mais!')
+            sys.exit()
+
+    elif opcao == 2:
+        add_or_del = int(input('Deseja adicionar ou Remover produtos? ( 1 ) para Adicionar e ( 2 ) para Remover '))
+
+        if add_or_del == 1:
+            while True:
+                produto = input('Digite o produto que deseja adicionar: (Para sair pressione "X") ').upper()
+
+                if produto == "X":
+                    start()
+
+                else:
+                    lista_atual.append(produto.capitalize())
+
+        else:
+            while True:
+                produto = input('Digite o produto que deseja excluir: (Para sair pressione "X") ').upper()
+
+                if produto == "X":
+                    start()
+
+                else:
+                    lista_atual.remove(produto.capitalize())
+
+    elif opcao == 3:
+        print(f'Você possui um total de {len(todas_listas)} lista')
+        print(f'Selecione as lista que deseja{lista_atual}')
+
+    elif opcao == 4:
         print('Finalizando programa, até mais!')
         sys.exit()
 
-elif opcao == 2:
-    print(todas_listas)
 
-elif opcao == 3:
-    produto = input('Digite o produto que deseja adicionar: ')
-    lista_atual.append(produto.capitalize())
+while invalid_input:
     start()
-
-elif opcao == 4:
-    print("a")
-
 
 
